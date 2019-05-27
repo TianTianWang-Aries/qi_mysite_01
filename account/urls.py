ab3d4c7a7ba2d9d69825470fd7d65d1951ce2589
wangtt <442091019@qq.com>
2019-05-27 15:46:40 +0800

    修改密码

--- account/urls.py
+++ account/urls.py
@@ -14,4 +14,11 @@ urlpatterns = [
     path('login/', auth_views.login, name = 'user_login' ),      # django的内置登录
     path('new-login/', auth_views.login, {"template_name": "account/login.html"}),
     path('logout/', auth_views.logout, {"template_name": "account/logout.html"}, name = 'user_logout'),
+    path('register/', views.register, name = "user_register"),
+
+    path('password-change/', auth_views.password_change,{"post_change_redirect": "account/password-change-done"}, name = 'password_change'),
+    path('password-change-done/', auth_views.password_change_done, name = 'password_change_done'),
+
+
+
 ]
